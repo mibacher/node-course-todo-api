7c6fb4c1cf75f6233727df7b5bf4c88ebbc3b9a9
mibacher <mibacher13@gmail.com>
2017-05-30 16:45:32 -0400

    Test Post /todos route

--- server/server.js
+++ server/server.js
@@ -17,12 +17,14 @@ app.post('/todos', (req, res) => {
   });
 
   todo.save().then((doc) => {
-    res.status(400).send(doc);
+    res.send(doc);
   }, (e) => {
-    res.send(e);
+    res.status(400).send(e);
   });
 });
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
+
+module .exports = {app};
